9cf5c606bc3c44a24e79a82c6db34355877c2334
militoj <militoj@gmail.com>
2017-03-02 18:17:43 -0600

    RectArea class is now working correctly, with two scanners asking for width and height of the rectangle

--- src/org/launchcode/java/demos/java4python/RectArea.java
+++ src/org/launchcode/java/demos/java4python/RectArea.java
@@ -1,7 +1,22 @@
 package org.launchcode.java.demos.java4python;
+import java.util.Scanner;
+
 
 /**
  * Created by johnmilito on 3/2/17.
  */
 public class RectArea {
+    public static void main(String[] args) {
+        Scanner in;
+
+        in = new Scanner(System.in);
+        System.out.println("What is the height of your rectangle:");
+        float height = in.nextFloat();
+        System.out.println("What is the width of your rectangle:");
+        float width = in.nextFloat();
+
+
+        System.out.println("The area of your triangle is: " + height * width);
+    }
+
 }
