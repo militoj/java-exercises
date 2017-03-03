90553dc54014a363892ea86a2367d8b99bf10270
militoj <militoj@gmail.com>
2017-03-02 18:29:55 -0600

    completed class

--- src/org/launchcode/java/demos/java4python/MilesPerGallon.java
+++ src/org/launchcode/java/demos/java4python/MilesPerGallon.java
@@ -1,7 +1,22 @@
 package org.launchcode.java.demos.java4python;
 
+import java.util.Scanner;
+
 /**
  * Created by johnmilito on 3/2/17.
  */
 public class MilesPerGallon {
+    public static void main(String[] args) {
+        Scanner in;
+
+        in = new Scanner(System.in);
+        System.out.println("How many miles have you driven:");
+        float miles = in.nextFloat();
+        System.out.println("How many gallons have you consumed:");
+        float gallons = in.nextFloat();
+
+
+        System.out.println("Your miles per gallon is: " + miles / gallons);
+    }
+
 }
