2038405c52f8cd5d7baa374edac03f737ad1526c
militoj <militoj@gmail.com>
2017-03-06 02:43:38 -0600

    taking a break but IDHashMap not yet working correctly

--- src/org/launchcode/java/demos/java4python/IDHashMap.java
+++ src/org/launchcode/java/demos/java4python/IDHashMap.java
@@ -17,40 +17,38 @@ import java.util.Scanner;
 
         public static void main(String[] args) {
 
-            HashMap<String, Double> students = new HashMap<>();
+            HashMap<Integer, String> students = new HashMap<>();
             Scanner in = new Scanner(System.in);
-            String newStudent;
+            Integer NewID;
 
             System.out.println("Enter your students (or ENTER to finish):");
 
             // Get student names and grades
             do {
 
-                System.out.print("Student: ");
-                newStudent = in.nextLine();
+                System.out.print("Student ID: ");
+                NewID = in.nextInteger();
 
-                if (!newStudent.equals("")) {
-                    System.out.print("Grade: ");
-                    Double newGrade = in.nextDouble();
-                    students.put(newStudent, newGrade);
+                if (!NewID.equals("")) {
+                    System.out.print("Student Name: ");
+                    String newStudent = in.next();
+                    students.put(NewID, newStudent);
 
                     // Read in the newline before looping back
                     in.nextLine();
                 }
 
-            } while(!newStudent.equals(""));
+            } while(!NewID.equals(""));
 
             // Print class roster
             System.out.println("\nClass roster:");
-            double sum = 0.0;
 
-            for (Map.Entry<String, Double> student : students.entrySet()) {
+            for (Map.Entry<Integer, String> student : students.entrySet()) {
                 System.out.println(student.getKey() + " (" + student.getValue() + ")");
                 sum += student.getValue();
             }
 
-            double avg = sum / students.size();
-            System.out.println("Average grade: " + avg);
+
         }
 
     }
