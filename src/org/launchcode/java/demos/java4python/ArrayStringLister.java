83ae8f92875be7372c545d00f55b7a4b62f74e69
militoj <militoj@gmail.com>
2017-03-06 02:00:50 -0600

    string list is working correctly

--- src/org/launchcode/java/demos/java4python/ArrayStringLister.java
+++ src/org/launchcode/java/demos/java4python/ArrayStringLister.java
@@ -1,7 +1,18 @@
 package org.launchcode.java.demos.java4python;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by johnmilito on 3/6/17.
  */
 public class ArrayStringLister {
+    public static void main(String[] args) {
+        List<String> animals = Arrays.asList("pig", "dog", "monkey",
+                "hippopotamus", "panda");
+
+        System.out.println(animals.get(2).length());
+
+    }
+
 }
