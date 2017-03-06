c29f1a42fb7141457a836d613194472d1c5d8394
militoj <militoj@gmail.com>
2017-03-06 02:23:24 -0600

    ArrayPrinter now prints every integer in array

--- src/org/launchcode/java/demos/java4python/ArrayPrinter.java
+++ src/org/launchcode/java/demos/java4python/ArrayPrinter.java
@@ -1,7 +1,16 @@
 package org.launchcode.java.demos.java4python;
 
+
 /**
  * Created by johnmilito on 3/6/17.
  */
 public class ArrayPrinter {
+    public static void main(String[] args) {
+        int[] array = {1, 1, 2, 3, 5, 8};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
 }
