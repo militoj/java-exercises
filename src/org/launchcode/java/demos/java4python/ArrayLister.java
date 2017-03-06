646b6f662c911ea24cd7ad2fcd93150fab7dff06
militoj <militoj@gmail.com>
2017-03-06 01:43:06 -0600

    ArrayLister now correctly printing sum of even integers in a list

--- src/org/launchcode/java/demos/java4python/ArrayLister.java
+++ src/org/launchcode/java/demos/java4python/ArrayLister.java
@@ -9,8 +9,17 @@ import java.util.List;
  */
 public class ArrayLister {
     public static void main(String[] args) {
-       List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+       List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20);
 
-       System.out.println(integers.get(2));
+        int sum = 0;
+
+        for (int i = 0; i < integers.size(); i++) {
+
+           if (integers.get(i) % 2 == 0) {
+               sum += integers.get(i);
+           }
+       }
+        System.out.println(sum);
+
     }
 }
