294c9a5fc579464e0636ccba8e34e7daf0dd6d7c
militoj <militoj@gmail.com>
2017-03-06 19:54:27 -0600

    separated calcSum into its own method that then is called in main

--- src/org/launchcode/java/demos/java4python/ArrayLister.java
+++ src/org/launchcode/java/demos/java4python/ArrayLister.java
@@ -9,17 +9,23 @@ import java.util.List;
  */
 public class ArrayLister {
     public static void main(String[] args) {
-       List<Integer> integers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20);
+       List<Integer> integers = Arrays.asList(2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 20);
 
+
+        System.out.println(calcSum(integers));
+
+    }
+
+    public static Integer calcSum(List<Integer> alist) {
         int sum = 0;
 
-        for (int i = 0; i < integers.size(); i++) {
+        for (int i = 0; i < alist.size(); i++) {
 
-           if (integers.get(i) % 2 == 0) {
-               sum += integers.get(i);
-           }
-       }
-        System.out.println(sum);
+            if (alist.get(i) % 2 == 0) {
+                sum += alist.get(i);
+            }
+        }
 
+        return sum;
     }
 }
