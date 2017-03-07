0806e98f90f89f023cc9d51bc21286871c62f246
militoj <militoj@gmail.com>
2017-03-06 19:55:39 -0600

    fixe this by switching the ID and student name around because the for loops checking if there was another student were comparing strings and the ID was an integer

--- src/org/launchcode/java/demos/java4python/IDHashMap.java
+++ src/org/launchcode/java/demos/java4python/IDHashMap.java
@@ -16,33 +16,33 @@ public class IDHashMap {
 
         public static void main(String[] args) {
 
-            HashMap<Integer, String> students = new HashMap<>();
+            HashMap<String, Integer> students = new HashMap<>();
             Scanner in = new Scanner(System.in);
-            Integer NewID;
+            String newStudent;
 
             System.out.println("Enter your students (or ENTER to finish):");
 
             // Get student names and grades
             do {
 
-                System.out.print("Student ID: ");
-                NewID = in.nextInt();
+                System.out.print("Student Name: ");
+                newStudent = in.nextLine();
 
-                if (!NewID.equals("")) {
-                    System.out.print("Student Name: ");
-                    String newStudent = in.next();
-                    students.put(NewID, newStudent);
+                if (!newStudent.equals("")) {
+                    System.out.print("Student ID: ");
+                    Integer newID = in.nextInt();
+                    students.put(newStudent, newID);
 
                     // Read in the newline before looping back
                     in.nextLine();
                 }
 
-            } while(!NewID.equals(""));
+            } while(!newStudent.equals(""));
 
             // Print class roster
             System.out.println("\nClass roster:");
 
-            for (Map.Entry<Integer, String> student : students.entrySet()) {
+            for (Map.Entry<String, Integer> student : students.entrySet()) {
                 System.out.println(student.getKey() + " (" + student.getValue() + ")");
             }
 
