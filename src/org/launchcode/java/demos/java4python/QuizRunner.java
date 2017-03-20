f5d9a9622e04ff05dee447cc73202ff95771b392
militoj <militoj@gmail.com>
2017-03-20 18:58:29 -0500

    created and messed around with QuizRunner class

---
+++ src/org/launchcode/java/demos/java4python/QuizRunner.java
@@ -0,0 +1,34 @@
+package org.launchcode.java.demos.java4python;
+
+import static java.lang.Boolean.FALSE;
+
+/**
+ * Created by johnmilito on 3/20/17.
+ */
+public class QuizRunner {
+
+    public void Question(int userAnswer) {
+        Boolean isAnswerCorrect = FALSE;
+
+        // do some crap
+
+    }
+
+    public boolean Quiz(int userAnswer) {
+        Boolean isAnswerCorrect = FALSE;
+
+        // do some crap
+
+        return isAnswerCorrect;
+    }
+
+
+    public static void main(String[] args) {
+
+
+
+
+
+
+        }
+    }
